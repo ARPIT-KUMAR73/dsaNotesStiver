2041e01ea9a9efbc3483f8e3f94c987ec5ddb22f
Arpit Kumar <akgate2022@gmail.com>
2025-12-24 22:40:30 +0530

    Improve explanation for maximum product subarray
    Refactor explanation for clarity and conciseness.

--- 01.Arrays/3.Hard/12.Maximum_product_subarray.cpp
+++ 01.Arrays/3.Hard/12.Maximum_product_subarray.cpp
@@ -10,7 +10,9 @@ Explanation: [2,3] has the largest product 6.
 
 APPROACH:
 
-To find the subarray with the largest product, we iterate through the array while keeping track of the current product. We maintain two variables: `ans` to store the maximum product found so far and `prdct` to store the current product. Since negative numbers can change the sign and potentially result in a larger product, we run the loop twice, once from left to right and once from right to left.
+To find the subarray with the largest product, we iterate through the array while keeping track of the current product.
+We maintain two variables: `ans` to store the maximum product found so far and `prdct` to store the current product. 
+Since negative numbers can change the sign and potentially result in a larger product, we run the loop twice, once from left to right and once from right to left.
 
 CODE:*/
 
